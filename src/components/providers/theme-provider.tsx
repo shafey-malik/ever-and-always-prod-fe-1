a6abb718e87bad2eb3b4b9dd1a3db047dae7e26d
shafey-malik <153776990+shafey-malik@users.noreply.github.com>
2026-01-27 22:47:55 +0530

    light theme

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import {ThemeProvider as NextThemesProvider} from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({children}: {children: React.ReactNode}) {
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
     return (
         <NextThemesProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
         >
